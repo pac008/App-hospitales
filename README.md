a4bc9389b51e08ef22b8271de15cac7262b5dad4
Miguel <miguelhalc@gmail.com>
2021-05-07 08:17:57 +0200

    Primer commit Diseño

---
+++ README.md
@@ -0,0 +1,10 @@
+# Adminpro
+
+Este es un proyecto de una aplicación web con angular para gestionar un hospital
+
+
+Ejecutar
+
+````````````
+npm i 
+````````````
---
+++ src/app/pages/notpagefound/notpagefound.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notpagefound',
+  templateUrl: './notpagefound.component.html',
+  styleUrls: [ './notpagefound.component.css' ]
+})
+export class NotpagefoundComponent {
+
+  year;
+  constructor() { 
+    this.year = new Date().getFullYear();
+  }
+
+
+}
